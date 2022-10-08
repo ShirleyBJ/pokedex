3abe2698d98367cf099beea5da54d289619d8c6c
ShirleyBJ <73275314+ShirleyBJ@users.noreply.github.com>
2022-10-08 22:32:40 +0200

    added  the navbar with icon

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,41 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import CatchingPokemonTwoToneIcon from "@mui/icons-material/CatchingPokemonTwoTone";
+import StarIcon from "@mui/icons-material/Star";
+import ForwardToInboxSharpIcon from "@mui/icons-material/ForwardToInboxSharp";
+
+export default function Navbar() {
+  return (
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" color="secondary">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <CatchingPokemonTwoToneIcon fontSize="large" />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Pokédex
+            </Typography>
+            <ForwardToInboxSharpIcon
+              fontSize="large"
+              style={{
+                margin: "15px",
+              }}
+            />
+            <StarIcon fontSize="large" />
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </>
+  );
+}
